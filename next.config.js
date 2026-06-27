e4a801f2ff1566f5f900ce47c8543ed4a5c064c4
Iahara <isharaktii@gmail.com>
2026-06-27 11:01:58 +0530

    Update and rename js to next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET || "mysecretkey"
+  }
+}
+module.exports = nextConfig
