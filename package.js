1281186ce9764fde3337ac18d4ee3120ccb40f53
Iahara <isharaktii@gmail.com>
2026-06-27 11:18:50 +0530

    Add package.json for project dependencies and scripts

---
+++ package.js
@@ -0,0 +1,28 @@
+{
+  "name": "galaxy-workers",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "postinstall": "prisma generate"
+  },
+  "dependencies": {
+    "next": "14.2.3",
+    "react": "18",
+    "react-dom": "18",
+    "@prisma/client": "^5.11.0",
+    "prisma": "^5.11.0",
+    "jsonwebtoken": "^9.0.0",
+    "bcryptjs": "^2.4.3",
+    "next-intl": "^3.5.0",
+    "three": "^0.162.0",
+    "@react-three/fiber": "^8.15.0",
+    "@react-three/drei": "^9.88.0",
+    "tailwindcss": "^3.4.0",
+    "react-hot-toast": "^2.4.1",
+    "axios": "^1.6.0",
+    "form-data": "^4.0.0"
+  }
+}
